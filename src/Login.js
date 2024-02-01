9c9a772ae687e7a75de250e8dcc1d3692f72b97d
sun <sun>
2024-02-02 08:24:13 +0900

    login_page

---
+++ src/Login.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+        <h1>ログインページ</h1>
+        <form>
+            <div>
+                <label>メールアドレス</label>
+                <input name="email" type="email" />
+            </div>
+            <div>
+                <label>パスワード</label>
+                <input name="password" type="パスワード" />
+            </div>
+            <button>ログイン</button>
+        </form>
+    
+    
+    </>
+  )
+}
+
+export default Login
